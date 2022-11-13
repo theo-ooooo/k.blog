dbd813b9a07d30c0d930ebb564f23c05950657eb
kkwondev <kkwoncokr@gmail.com>
2022-11-13 13:14:05 +0900

    cors setting

--- packages/blog-server/src/main.ts
+++ packages/blog-server/src/main.ts
@@ -14,7 +14,7 @@ const app = express();
 
 app.use(express.json());
 app.use(cookieParser());
-app.use(cors({ origin: true, credentials: true }));
+app.use(cors({ origin: ['http://localhost:3000', 'https://blog.kwkang.dev'], credentials: true }));
 
 app.use('/api/v1', apiV1Routes);
 
